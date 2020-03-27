631052180ccd82190c05a22c0256c2933cc396ba
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-27 20:56:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,4 +20,12 @@ def pop_with_args(array)
  end
 
  def shift_with_args(array)
-   array.shift(2)
+   array.shift(2) 
+ end
+
+ def using_concat(array1, array2)
+   array1.concat(array2) 
+ end
+
+ def using_insert(array, newElement)
+   array.insert( 1 , newElement)
